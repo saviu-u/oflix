e405d0a2a60d727e77b539575abb030cc3bae060
Sávio Cangussu <saviocangussu@hotmail.com>
2020-03-23 21:06:49 -0300

    Unfinished changes

--- src/main/java/com/squad4/oflix/controller/UsuariosController.java
+++
@@ -1,9 +0,0 @@
-package com.squad4.oflix.controller;
-
-
-import javax.servlet.annotation.WebServlet;
-@WebServlet(urlPatterns = {"/usuarios/*"})
-
-public class UsuariosController extends Controller {
-    
-}
--- src/main/java/com/squad4/oflix/model/Model.java
+++
@@ -1,9 +0,0 @@
-package com.squad4.oflix.model;
-
-/**
- *
- * @author savio
- */
-public class Model {
-    
-}
